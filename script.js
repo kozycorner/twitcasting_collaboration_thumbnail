dad6b24a95617645b7eab2013fa33a0d8a546a05
Tab <tiger0brother@gmmail.com>
2020-06-01 05:29:00 +0900

    Add fetch test

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 function load_img(){
 	const output = document.getElementById('thumbnail');
-//const base64 = document.getElementById('base64');
 
-var username = document.getElementById('username').value;
+	var username = document.getElementById('username').value;
 
-if(username==""){
-	username="null";
-}
+	if(username==""){
+		username="null";
+	}
 
-var url = 'https://apiv2.twitcasting.tv/users/'
-+ username
-+ '/live/thumbnail?size=large&position=latest';
+	var url = 'https://apiv2.twitcasting.tv/users/'
+	+ username + '/live/thumbnail?size=large&position=latest';
 
-var thumbnail = document.getElementById('thumbnail');
-thumbnail.setAttribute('src',url);
+	var thumbnail = document.getElementById('thumbnail');
+	thumbnail.setAttribute('src',url);
 
-var img = get_img(url);
+	var data={};
+
+	fetch("https://7ezd12td71.execute-api.us-east-2.amazonaws.com/",{
+	        method: "GET", // *GET, POST, PUT, DELETE, etc.
+	        mode: "no-cors", // no-cors, cors, *same-origin
+	        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+	        credentials: "include", // include, same-origin, *omit
+	        headers: {
+	            "Content-Type": "application/json; charset=utf-8",
+	            // "Content-Type": "application/x-www-form-urlencoded",
+	        },
+	        redirect: "follow", // manual, *follow, error
+	        referrer: "client", // no-referrer, *client
+	    })
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(myJson) {
+			console.log(JSON.stringify(myJson));
+		});
+
 }
 
 $(function(){
@@ -44,7 +62,7 @@ $(function(){
 
 		var i;
 
-		for(i=1;i++;i<=7){
+		for(i=1;i<=7;i++){
 			document.querySelector("#out div:nth-child("+i+")").style.display=(i > people ? "none" : "block");
 		}
 	}); 
