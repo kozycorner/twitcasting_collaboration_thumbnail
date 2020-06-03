05076d3e993c8ab12bd72ba34be34f7b700ec668
Tab <tiger0brother@gmmail.com>
2020-06-04 01:12:45 +0900

    新しいAPIに変更

--- script.js
+++ script.js
@@ -15,24 +15,27 @@ function load_img(){
 
 	var data={};
 
-	fetch("https://7ezd12td71.execute-api.us-east-2.amazonaws.com/",{
-	        method: "GET", // *GET, POST, PUT, DELETE, etc.
-	        mode: "no-cors", // no-cors, cors, *same-origin
-	        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-	        credentials: "include", // include, same-origin, *omit
-	        headers: {
-	            "Content-Type": "application/json; charset=utf-8",
-	            // "Content-Type": "application/x-www-form-urlencoded",
-	        },
-	        redirect: "follow", // manual, *follow, error
-	        referrer: "client", // no-referrer, *client
-	    })
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(myJson) {
-			console.log(JSON.stringify(myJson));
-		});
+	fetch("https://rdqtkr8gcd.execute-api.us-east-2.amazonaws.com/",{
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+//        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//        credentials: "include", // include, same-origin, *omit
+        headers: {
+            //"Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+//            "Access-Control-Allow-Origin": "*",
+//            "Access-Control-Request-Method": "GET",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "client", // no-referrer, *client
+
+    })
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
+	});
 
 }
 
